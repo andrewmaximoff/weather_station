78a0860c2d4e2379f3e4bdfd36e4341989a3483d
qiwiGremL1n <echoes@ex.ua>
2018-04-12 13:37:06 +0300

    fix

--- temp_4_p2.py
+++ temp_4_p2.py
@@ -21,7 +21,7 @@ def get_val():
     data = get_data()
     temperature = get_float(data, 0)
     luminosity = get_float(data, 1)
-    return ' '.join([str(temperature), str(luminosity)])
+    return '{} {}'.format(temperature, luminosity)
 
 
 if __name__ == '__main__':
