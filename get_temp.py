7df9ea58e2109780ea6b4c658dc238895d54e6c9
qiwiGremL1n <echoes@ex.ua>
2018-04-12 13:34:17 +0300

    fix

--- get_temp.py
+++ get_temp.py
@@ -1,7 +1,13 @@
 from subprocess import Popen, PIPE
 import sys
 
+
+def main():
+    with Popen(['/usr/bin/python', '-u', 'temp_4_p2.py'], stdout=PIPE, universal_newlines=True) as process:
+        indicators = process.stdout
+    return str(indicators).split(' ')
+
+
 if __name__ == '__main__':
     with Popen(['/usr/bin/python', '-u', 'temp_4_p2.py'], stdout=PIPE, universal_newlines=True) as process:
-        for line in process.stdout:
-            print(line, end='')
+        temp = process.stdout
--- temp_4_p2.py
+++ temp_4_p2.py
@@ -21,7 +21,7 @@ def get_val():
     data = get_data()
     temperature = get_float(data, 0)
     luminosity = get_float(data, 1)
-    return [temperature, luminosity]
+    return ' '.join([temperature, luminosity])
 
 
 if __name__ == '__main__':
