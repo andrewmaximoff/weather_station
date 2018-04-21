a515ed85aef97936e7f1f4a7e082a75a70445e33
Andrew Maximov <echoes@ex.ua>
2018-04-21 23:12:08 +0300

    Fix bugs

--- bot/temp.py
+++ bot/temp.py
@@ -8,9 +8,9 @@ pin = 17
 
 def temperature():
     _, result = Adafruit_DHT.read_retry(sensor, pin)
-    return 'Temp={:.2f}'.format(result)
+    return 'Temp={:0.1f}'.format(result)
 
 
 def humidity():
     result, _ = Adafruit_DHT.read_retry(sensor, pin)
-    return 'Humidity={:.2f}'.format(result)
+    return 'Humidity={:0.1f}'.format(result)
