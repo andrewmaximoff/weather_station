2097c5dab5ef8b7aa1585fd7178108bd7ff6ae68
Andrew Maximov <echoes@ex.ua>
2018-04-21 23:28:55 +0300

    Fix bugs

--- bot/bar.py
+++ bot/bar.py
@@ -7,3 +7,11 @@ def barometer():
     output = p.stdout.readlines()
     output = [i.decode() for i in output]
     return ''.join(output)
+
+
+def tempr():
+    command = ["python", "test_bar.py"]
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output = p.stdout.readlines()
+    output = [i.decode() for i in output]
+    return output[2]
